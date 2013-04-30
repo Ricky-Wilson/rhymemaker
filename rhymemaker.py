6a008c76414d6aa94828208f22bff45dbf96bdf8
Yat Choi <yatchoi1@gmail.com>
2013-04-30 14:52:16 -0400

    Better rhyme matching logic, especially for near rhymes. Multiple words can be used for input. Kind of hacky code for similar syllables though

--- rhymemaker.py
+++ rhymemaker.py
@@ -1,17 +1,37 @@
 import wordsmith
 
 def nearSyllableScore(syllable1, syllable2):
-	if (syllable1[0] == syllable2[0]) and (syllable1[1] == syllable2[1]):
-		return 1
-	elif (syllable1[0] != 1 and syllable2[0] != 1):
-		if ((syllable1[1][:2] == 'IH' and syllable2[1][:2] == 'AH') or (syllable1[1][:2] == 'AH' and syllable2[1][:2] == 'IH')):
-			return 0.5
+	if ((syllable1[0] == syllable2[0]) and (syllable1[1] == syllable2[1])):
+		return 1.3
 
-	return -1
+	score = -1
+	if (syllable1[1][:2] == syllable2[1][:2]):
+		score = 1
+	elif ((syllable1[1][:2] == 'IH' and syllable2[1][:2] == 'AH') or (syllable1[1][:2] == 'AH' and syllable2[1][:2] == 'IH')):
+		score = 0.5
+	elif ((syllable1[1][:2] == 'IH' and syllable2[1][:2] == 'IY') or (syllable1[1][:2] == 'IY' and syllable2[1][:2] == 'IH')):
+		score = 0.5
+	elif ((syllable1[1][:2] == 'AA' and syllable2[1][:2] == 'AO') or (syllable1[1][:2] == 'AO' and syllable2[1][:2] == 'AA')):
+		score = 0.5
+	elif ((syllable1[1][:2] == 'AH' and syllable2[1][:2] == 'AO') or (syllable1[1][:2] == 'AO' and syllable2[1][:2] == 'AH')):
+		score = 0.5
+	elif ((syllable1[1][:2] == 'AH' and syllable2[1][:2] == 'AY') or (syllable1[1][:2] == 'AY' and syllable2[1][:2] == 'AH')):
+		score = 0.5
+	elif ((syllable1[1][:2] == 'AY' and syllable2[1][:2] == 'AE') or (syllable1[1][:2] == 'AE' and syllable2[1][:2] == 'AY')):
+		score = 0.5
+
+	if (syllable1[0] == 1 and syllable2[0] == 1):
+		score *= 0.25
+	elif (syllable1[0] == 1 or syllable2[0] == 1):
+		score *= 0.5
+	elif(syllable1[0] != syllable2[0]):
+		score *= 0.75
+
+	return score
 
 def nearConsonantScore(cons1, cons2):
 	if (cons1 == cons2):
-		return 1
+		return 1.5
 	else:
 		return 0
 
@@ -29,33 +49,26 @@ def rhyme(word, maxnum):
 
 	return output
 
-def getNearRhymes(inWord):
+def getNearRhymes(inputString):
 	rhymes = []
-	if (not inWord in wordsmith.dictionary):
-		return []
+	pron1 = tokenize(inputString)
 
 	for word in wordsmith.dictionary.keys():
-		score = nearRhymeScore(word, inWord)
+		if inputString == word:
+			continue
+		elif "'" in word:
+			continue
+
+		pron2 = tokenize(word)
+		
+		score = nearRhymeScore(pron1, pron2)
 		if score > 0:
 			rhymes.append((word, score))
 
 	
 	return sorted(rhymes, key=lambda t: t[1], reverse=True)
 
-def nearRhymeScore(word1, word2):
-	if word1 == word2:
-		return -1
-
-	pron1 = wordsmith.getPron(word1)
-	pron2 = wordsmith.getPron(word2)
-
-	if not pron1:
-		print ("Could not recognize " + word1)
-		return -1
-
-	if not pron2:
-		print ("Could not recognize " + word2)
-		return -1
+def nearRhymeScore(pron1, pron2):
 
 	stress1 = wordsmith.getStress(pron1)
 	stress2 = wordsmith.getStress(pron2)
@@ -63,9 +76,9 @@ def nearRhymeScore(word1, word2):
 	if not stress1 or not stress2:
 		return -1
 
-	if (nearSyllableScore(stress1[-1], stress2[-1]) < 0):
-		# print ("Last syllable not similar enough")
-		return -1
+	# if (nearSyllableScore(stress1[-1], stress2[-1]) < 0):
+	# 	# print ("Last syllable not similar enough")
+	# 	return -1
 
 	match = 0
 
@@ -79,19 +92,22 @@ def nearRhymeScore(word1, word2):
 		pron2 = tempPron
 
 	for i in range(1,len(stress1)+1):
-		# Check same stress pattern of word
-		if (stress1[-i][0] != stress2[-i][0]):
-			# print("Syllable stressing is different on syllable #%s" % -i)
-			match = -1
-			break
+
+		# # Check same stress pattern of word
+		# if (stress1[-i][0] != stress2[-i][0]):
+		# 	# print("Syllable stressing is different on syllable #%s" % -i)
+		# 	match = -1
+		# 	break
+
 		# Check that important stresses rhyme
-		elif ((stress1[-i][0] > 0) and (stress1[-i][1] != stress2[-i][1])):
+		score = nearSyllableScore(stress1[-i], stress2[-i])
+		if (score < 0):
 			# print("Important Syllable %s does not rhyme" % -i)
 			match = -1
 			break
 		# Check surrounding consonants for closeness
 		else:
-			match += nearSyllableScore(stress1[-i], stress2[-i])
+			match += score
 			if (stress1[-i][2] > 0) and (stress2[-i][2] > 0) and (stress1[-i][2] < len(pron1) - 1) and (stress2[-i][2] < len(pron2)-1):
 				index1 = stress1[-i][2]
 				index2 = stress2[-i][2]
@@ -100,6 +116,28 @@ def nearRhymeScore(word1, word2):
 
 	return match
 
+def tokenize(word):
+	pron = []
 
+	wordsplit = word.split()
+	for w in wordsplit:
+		if (not w in wordsmith.dictionary):
+			return []
+		else:
+			pron.extend(wordsmith.getPron(w))
+
+	return pron
+
+def check(inputString, word):
+	if inputString == word:
+		return -1
+
+	pron1 = tokenize(inputString)
+	pron2 = tokenize(word)
+
+	print pron1
+	print pron2
+		
+	return nearRhymeScore(pron1, pron2)
 
 
