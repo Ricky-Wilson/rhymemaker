6d6eb7bdbcc8e518bb51d9d3d102fc16a20995f1
Yat Choi <yatchoi1@gmail.com>
2013-04-30 20:02:29 -0400

    Tweaked scores

--- application.py
+++ application.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return render_template('home.html', rhymes=[])
+	return render_template('home.html', word=[], rhymes=[])
 
 @app.route('/rhyme')
 def ajax_rhyme():
 	word = request.args.get('word','')
-	rhymes = rhymemaker.rhyme(word,20)
-	return render_template('home.html', rhymes=rhymes)
+	rhymes = rhymemaker.rhyme(word,75)
+	return render_template('home.html', word=word, rhymes=rhymes)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- rhymemaker.py
+++ rhymemaker.py
@@ -2,40 +2,50 @@ import wordsmith
 
 def nearSyllableScore(syllable1, syllable2):
 	if ((syllable1[0] == syllable2[0]) and (syllable1[1] == syllable2[1])):
-		return 1.3
+		return 2
 
 	score = -1
 	if (syllable1[1][:2] == syllable2[1][:2]):
-		score = 1
+		score = 1.8
 	elif ((syllable1[1][:2] == 'IH' and syllable2[1][:2] == 'AH') or (syllable1[1][:2] == 'AH' and syllable2[1][:2] == 'IH')):
 		score = 0.5
 	elif ((syllable1[1][:2] == 'IH' and syllable2[1][:2] == 'IY') or (syllable1[1][:2] == 'IY' and syllable2[1][:2] == 'IH')):
-		score = 0.5
+		score = 0.3
 	elif ((syllable1[1][:2] == 'AA' and syllable2[1][:2] == 'AO') or (syllable1[1][:2] == 'AO' and syllable2[1][:2] == 'AA')):
 		score = 0.5
 	elif ((syllable1[1][:2] == 'AH' and syllable2[1][:2] == 'AO') or (syllable1[1][:2] == 'AO' and syllable2[1][:2] == 'AH')):
 		score = 0.5
 	elif ((syllable1[1][:2] == 'AH' and syllable2[1][:2] == 'AY') or (syllable1[1][:2] == 'AY' and syllable2[1][:2] == 'AH')):
-		score = 0.5
+		score = 0.2
 	elif ((syllable1[1][:2] == 'AY' and syllable2[1][:2] == 'AE') or (syllable1[1][:2] == 'AE' and syllable2[1][:2] == 'AY')):
 		score = 0.5
+	elif ((syllable1[1][:2] == 'AO' and syllable2[1][:2] == 'AE') or (syllable1[1][:2] == 'AE' and syllable2[1][:2] == 'AO')):
+		score = 0.5
+	elif ((syllable1[1][:2] == 'EY' and syllable2[1][:2] == 'AH') or (syllable1[1][:2] == 'AH' and syllable2[1][:2] == 'EY')):
+		score = 0.25
 
 	if (syllable1[0] == 1 and syllable2[0] == 1):
 		score *= 0.25
 	elif (syllable1[0] == 1 or syllable2[0] == 1):
-		score *= 0.5
-	elif(syllable1[0] != syllable2[0]):
 		score *= 0.75
+	elif(syllable1[0] != syllable2[0]):
+		score *= 0.8
 
 	return score
 
 def nearConsonantScore(cons1, cons2):
 	if (cons1 == cons2):
-		return 1.5
+		return 1.2
+	elif ((cons1 == 'T' and cons2 == 'D') or (cons1 == 'D' and cons2 == 'T')):
+		return 1
+	elif ((cons1 == 'S' and cons2 == 'Z') or (cons1 == 'Z' and cons2 == 'S')):
+		return 1
+	elif ((cons1 == 'S' and cons2 == 'T') or (cons1 == 'T' and cons2 == 'S')):
+		return 0.4
 	else:
 		return 0
 
-def rhyme(word, maxnum):
+def rhyme(word, maxnum=50):
 	rhymes = getNearRhymes(word)
 
 	output = []
@@ -76,10 +86,6 @@ def nearRhymeScore(pron1, pron2):
 	if not stress1 or not stress2:
 		return -1
 
-	# if (nearSyllableScore(stress1[-1], stress2[-1]) < 0):
-	# 	# print ("Last syllable not similar enough")
-	# 	return -1
-
 	match = 0
 
 	if (len(stress1) > len(stress2)):
@@ -92,27 +98,37 @@ def nearRhymeScore(pron1, pron2):
 		pron2 = tempPron
 
 	for i in range(1,len(stress1)+1):
+		
 
-		# # Check same stress pattern of word
-		# if (stress1[-i][0] != stress2[-i][0]):
-		# 	# print("Syllable stressing is different on syllable #%s" % -i)
-		# 	match = -1
-		# 	break
+		# Check same stress pattern of word
+		if (stress1[-i][0] == stress2[-i][0]):
+			match+= 1
 
-		# Check that important stresses rhyme
 		score = nearSyllableScore(stress1[-i], stress2[-i])
 		if (score < 0):
-			# print("Important Syllable %s does not rhyme" % -i)
 			match = -1
 			break
-		# Check surrounding consonants for closeness
+
 		else:
-			match += score
 			if (stress1[-i][2] > 0) and (stress2[-i][2] > 0) and (stress1[-i][2] < len(pron1) - 1) and (stress2[-i][2] < len(pron2)-1):
 				index1 = stress1[-i][2]
 				index2 = stress2[-i][2]
 
-				match += nearConsonantScore(pron1[index1+1], pron2[index2+1])
+				# Check surrounding consonants for closeness
+				consonantScore = nearConsonantScore(pron1[index1+1], pron2[index2+1])
+				if i == 1:
+					score *= 1.5
+					if score > 1:
+						consonantScore *= 2.5
+				# if i == 2:
+				# 	consonantScore *=1.5
+				print stress1[-i]
+				print stress2[-i]
+				print score
+				print consonantScore
+
+				match+=score
+				match+= consonantScore
 
 	return match
 
